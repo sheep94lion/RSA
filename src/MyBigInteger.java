bed4304e8732170e34c1a7ccc6d2a1a774c9e9c1
sheep94lion_local <zhaoyi.yuan31@gmail.com>
2017-10-31 22:40:49 +0800

    init wow~

---
+++ src/MyBigInteger.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class MyBigInteger {
+    private byte[] bytesBI;
+
+    public MyBigInteger(byte[] bytes){
+        this.bytesBI = bytes;
+    }
+
+    public static void main(String[] args){
+
+    }
+
+    public static MyBigInteger randomPrime(int bitlength, Random r) {
+
+    }
+}
