fdb4f2a17f5e96e1a237584c00942e679c782e77
sheep94lion_local <zhaoyi.yuan31@gmail.com>
2017-11-02 14:48:30 +0800

    generate random prime

--- src/MyBigInteger.java
+++ src/MyBigInteger.java
@@ -11,7 +11,28 @@ public class MyBigInteger {
 
     }
 
-    public static MyBigInteger randomPrime(int bitlength, Random r) {
+    public static boolean isPrime(MyBigInteger myBI) {
+        return true;
+    }
 
+    public MyBigInteger addInt(int add) {
+        return this;
+    }
+
+    public static MyBigInteger randomPrime(int bitlength, Random r) {
+        byte[] randomBytes = new byte[bitlength];
+        r.nextBytes(randomBytes);
+        if (randomBytes[randomBytes.length - 1] % 2 == 0) {
+            randomBytes[randomBytes.length - 1] += 1;
+        }
+        while (true) {
+            MyBigInteger myBI = new MyBigInteger(randomBytes);
+            if (isPrime(myBI)) {
+                return myBI;
+            } else {
+                myBI.addInt(1);
+                //TODO if it reach max
+            }
+        }
     }
 }
